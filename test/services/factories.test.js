941e23a586591783db612a0c0b07fc8f93afa731
Gabriel L. Maljkovich <gabriel.l.maljkovich@gmail.com>
2019-06-09 17:57:27 -0300

    Try closing the connection on service tests

--- test/services/factories.test.js
+++ test/services/factories.test.js
@@ -1,8 +1,13 @@
 const app = require('../../src/app');
+const mongoose = require('mongoose');
 
 describe('\'factories\' service', () => {
   it('registered the service', () => {
     const service = app.service('factories');
     expect(service).toBeTruthy();
+  });
+  afterAll(async () => {
+    await mongoose.disconnect();
+    await mongoose.connection.close();
   });
 });
