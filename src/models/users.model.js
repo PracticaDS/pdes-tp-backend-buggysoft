124f973d4e73cd11893cb57b69caa403b81c9153
Juan Manuel Rodriguez Defago <juanma.rdefago@gmail.com>
2019-06-09 00:31:57 -0300

    Added error hook to create user if it doesn't exist

--- src/models/users.model.js
+++ src/models/users.model.js
@@ -2,6 +2,7 @@ module.exports = function (app) {
   const mongooseClient = app.get('mongooseClient');
   const { Schema } = mongooseClient;
   const users = new Schema({
+    _id: { type: String, required: true },
     username: { type: String, required: true }
   }, {
     timestamps: true
--- src/services/users/users.hooks.js
+++ src/services/users/users.hooks.js
@@ -24,7 +24,30 @@ module.exports = {
   error: {
     all: [],
     find: [],
-    get: [],
+    get: [
+      function(context) {
+        const service = context.app.service('/users');
+
+        return new Promise(function(resolve, reject) {
+          function processResponse(response) {
+            if (response) {
+              context.result = response;
+              context.error = {};
+              context.statusCode = 200;
+            }
+            resolve(context);
+          }
+
+          if(!context.result) {
+            service.create({ _id: context.id, username: context.id }, context.params)
+              .then(response => processResponse(response))
+              .catch(e => {
+                reject(e);
+              });
+          }
+        });
+      }
+    ],
     create: [],
     update: [],
     patch: [],
