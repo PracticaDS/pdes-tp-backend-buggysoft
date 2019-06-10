39a8608cf9a958a4fb3955f63cac90de6f41092f
Gabriel L. Maljkovich <gabriel.l.maljkovich@gmail.com>
2019-06-09 21:36:07 -0300

    [#4] Add prometheus basic logging

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,5 +1,14 @@
+const requestCounter = require('./requestCounter');
+const requestDuration = require('./requestDuration');
+
 // eslint-disable-next-line no-unused-vars
 module.exports = function (app) {
   // Add your custom middleware here. Remember that
   // in Express, the order matters.
+  const Prometheus = app.get('Prometheus');
+  const { collectDefaultMetrics } = Prometheus;
+  // Probe every 5th second.
+  collectDefaultMetrics({ timeout: 5000 });
+  requestDuration(app);
+  requestCounter(app);
 };
