789f21a76ba4578fab5ba4bfbbb0c043f59edf7f
Gabriel L. Maljkovich <gabriel.l.maljkovich@gmail.com>
2019-06-29 20:34:34 -0300

    remove file

--- docker-push.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker build -t pdes-tp-backend-buggysoft .
-docker tag pdes-tp-backend-buggysoft:latest glmaljkovich/pdes-tp-backend-buggysoft
-docker push glmaljkovich/pdes-tp-backend-buggysoft
